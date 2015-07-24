b688a4500f24dc5354e2cb719fade30f9b8d18bc
Abzal Serekov <abzal.serekov@gmail.com>
2015-07-24 17:14:08 -0500

    jquery is added in events of showItem template for hiding/showing new comment body

--- client/templates/items/showItem.js
+++ client/templates/items/showItem.js
@@ -3,3 +3,14 @@ Template.showItem.helpers({
 		return Items.findOne({});
 	}
 });
+
+Template.showItem.events({
+  "click #add-comment":function(event, template){
+    var $commentBody = $("#new-comment");
+    if ($commentBody.is(":visible")) {
+    	$commentBody.hide();
+    } else {
+    	$commentBody.show();
+    }
+  }
+});
