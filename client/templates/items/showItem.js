38a7e152f8916f52bd3bfa14e0fca19ce67131fa
Abzal Serekov <abzal.serekov@gmail.com>
2015-07-24 19:31:12 -0500

    cancel button hides new comment form

--- client/templates/items/showItem.js
+++ client/templates/items/showItem.js
@@ -6,11 +6,14 @@ Template.showItem.helpers({
 
 Template.showItem.events({
   "click #add-comment":function(event, template){
-    var $commentBody = $("#new-comment");
+    var $commentBody = template.$("#new-comment");
     if ($commentBody.is(":visible")) {
     	$commentBody.hide();
     } else {
     	$commentBody.show();
     }
+  },
+  "click #cancel-button": function (event, template) {
+    template.$("#new-comment").hide();  	
   }
 });
