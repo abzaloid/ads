1332eca7cca2aa10d382a91e0fd37c65d198c7c1
Abzal Serekov <abzal.serekov@gmail.com>
2015-07-28 03:03:13 -0500

    added a way to select option: buy or sell

--- client/templates/includes/header.js
+++ client/templates/includes/header.js
@@ -3,3 +3,20 @@ Template.header.helpers({
     return Categories.find();
   }
 });
+
+Template.header.events({
+	'click #sell': function (event, template) {
+		$("#sell").css("text-decoration", "none");
+		$("#sell").css("color", "white");
+		$("#buy").css("color", "#aaa");
+		$("#buy").css("font-size", "0.8em");
+		$("#sell").css("font-size", "1.2em");
+	},
+	'click #buy': function (event, template) {
+		$("#buy").css("text-decoration", "none");		
+		$("#buy").css("color", "white");
+		$("#sell").css("color", "#aaa");
+		$("#buy").css("font-size", "1.2em");
+		$("#sell").css("font-size", "0.8em");
+	},
+});
