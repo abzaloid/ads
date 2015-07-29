ff66d25473920775309c2c7c12b0cbd538a97bd0
Abzal Serekov <abzal.serekov@gmail.com>
2015-07-29 14:31:45 -0500

    added test (check) form

--- client/templates/sell/sell.js
+++ client/templates/sell/sell.js
@@ -15,8 +15,40 @@ Template.sell.events({
 			description: $(e.target).find('[name=description]').val(),
 		};
 
+		//
+		//	Testing if all fields are filled in a form
+		//
+
+
+		$(e.target).find('[name=name]').parent().find("small").hide();
+		$(e.target).find('[name=price]').parent().find("small").hide();
+		$(e.target).find('[name=description]').parent().find("small").hide();
+		$(e.target).find('[name=name]').parent().find("small").hide();
+			$("ul.sell-categories").parent().find("small").hide();
+
+
+		var correct_form = true;
+
+		if (sell_item.name.length == 0) {
+			$(e.target).find('[name=name]').parent().find("small").show();
+			correct_form = false;
+		}
+		if (sell_item.price.length == 0) {
+			$(e.target).find('[name=price]').parent().find("small").show();
+			correct_form = false;
+		}
+		if (sell_item.description.length == 0) {
+			$(e.target).find('[name=description]').parent().find("small").show();
+			correct_form = false;
+		}
+
+		if (sell_item.condition.length == 0) {
+			correct_form = false;
+		}
+
+
 		var $categories = $("ul.sell-categories").find("li > a.sell-success");
-		if ($categories) {
+		if ($categories & $categories.length > 0) {
 			var categories_values = [];
 			var cat_len = $categories.length;
 			for (var i = 0; i < cat_len; i++) {
@@ -32,12 +64,19 @@ Template.sell.events({
 				categories: categories_values,
 			});
 		} else {
-			alert("Please choose at least one category");
+			$("ul.sell-categories").parent().find("small").show();
+			correct_form = false;
+		}
+
+		if (! correct_form) {
+			alert("Please fill all fields");
 			return false;
 		}
 
-		console.log("Name = " + sell_item.name);
-		console.log("Condition = " + sell_item.condition);
+
+		//
+		//	call Meteor Server Side for Inserting new Sell Item
+		//
 
 		Meteor.call('sellItemInsert', sell_item, function (error, result) {
 			if (error)
