20225aedf3b094a94787c54cc94689b0a144b9a6
Abzal Serekov <abzal.serekov@gmail.com>
2015-08-04 01:04:37 -0500

    search input handle events is started

--- client/templates/includes/header.js
+++ client/templates/includes/header.js
@@ -5,5 +5,9 @@ Template.header.helpers({
 });
 
 Template.header.events({
-	
+	'keyup #search-input': function (event) {
+		var search_text = $("#search-input").val();
+
+		$("#hello").text(search_text);
+	}
 });
