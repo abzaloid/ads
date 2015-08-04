fe91b2cc1e37e8896ca5a09beb6c34487c13d4ba
Abzal Serekov <abzal.serekov@gmail.com>
2015-08-04 00:32:39 -0500

    removed buy/sell option

--- client/templates/includes/header.js
+++ client/templates/includes/header.js
@@ -5,18 +5,5 @@ Template.header.helpers({
 });
 
 Template.header.events({
-	'click #sell': function (event, template) {
-		$("#sell").css("text-decoration", "none");
-		$("#sell").css("color", "white");
-		$("#buy").css("color", "#aaa");
-		$("#buy").css("font-size", "0.8em");
-		$("#sell").css("font-size", "1.2em");
-	},
-	'click #buy': function (event, template) {
-		$("#buy").css("text-decoration", "none");		
-		$("#buy").css("color", "white");
-		$("#sell").css("color", "#aaa");
-		$("#buy").css("font-size", "1.2em");
-		$("#sell").css("font-size", "0.8em");
-	},
+	
 });
