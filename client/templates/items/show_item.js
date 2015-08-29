7d868add6a31f0999697e6fb0ee3d58270cd6883
Abzal Serekov <abzal.serekov@gmail.com>
2015-08-30 00:03:34 +0600

    currently comments will be replaced by DISQUSS

--- client/templates/items/show_item.js
+++ client/templates/items/show_item.js
@@ -1,13 +1,13 @@
 Template.showItem.events({
-  "click #add-comment":function(event, template){
-    var $commentBody = template.$("#new-comment");
-    if ($commentBody.is(":visible")) {
-    	$commentBody.hide();
-    } else {
-    	$commentBody.show();
-    }
-  },
-  "click #cancel-button": function (event, template) {
-    template.$("#new-comment").hide();  	
-  }
+  // "click #add-comment":function(event, template){
+  //   var $commentBody = template.$("#new-comment");
+  //   if ($commentBody.is(":visible")) {
+  //   	$commentBody.hide();
+  //   } else {
+  //   	$commentBody.show();
+  //   }
+  // },
+  // "click #cancel-button": function (event, template) {
+  //   template.$("#new-comment").hide();  	
+  // }
 });
